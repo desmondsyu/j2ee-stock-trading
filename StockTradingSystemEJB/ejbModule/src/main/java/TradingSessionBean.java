95ad907ab5299720e1e34a33ffb5015f380ecc7e
desmondsyu <desmondzkx@gmail.com>
2025-03-18 13:55:22 -0400

    init

---
+++ StockTradingSystemEJB/ejbModule/src/main/java/TradingSessionBean.java
@@ -0,0 +1,27 @@
+package src.main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.ejb.LocalBean;
+import jakarta.ejb.Stateful;
+
+/**
+ * Session Bean implementation class TradingSessionBean
+ */
+@Stateful
+@LocalBean
+public class TradingSessionBean {
+
+    /**
+     * Default constructor. 
+     */
+    public TradingSessionBean() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    private List<String> watchList = new ArrayList<>();
+    
+    
+
+}
